7476004a806a31a92577eaffb7444fd0cba5ac68
Poupichette <zakia.hachemane6@orange.fr>
2026-07-18 23:19:46 +0200

    Utilise la configuration pour la connexion MySQL

--- app/core/Database.php
+++ app/core/Database.php
@@ -7,7 +7,13 @@ final class Database
         if (self::$pdo === null) {
             $config = require __DIR__ . '/../config/config.php';
             $db = $config['db'];
-            $dsn = "mysql:host=localhost;port=3308;dbname=vite_gourmand;charset=utf8mb4";
+            $dsn = sprintf(
+                'mysql:host=%s;port=%s;dbname=%s;charset=%s',
+                $db['host'],
+                $db['port'],
+                $db['name'],
+                $db['charset']
+            );
             self::$pdo = new PDO($dsn, $db['user'], $db['password'], [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
